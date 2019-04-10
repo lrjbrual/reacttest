8a9604c531f377d62d826d4a1163cca7088d3a52
Ryan Jeff Brual <lrjbrual@live.com>
2019-04-10 14:03:38 +0200

    update reactjs for alchie

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #for reactjs
+  get "/posts/new", to: 'pages#home'
+  get "posts/:id", to: 'pages#home'
+
+  # API to handle reactjs
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :posts, only: [:index, :show, :create]
+    end
+  end
+
+
 end
